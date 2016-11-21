edf95d081a6048d0f3c94469309e176d4de5e499
Melissa Rodriguez <olenellina@gmail.com>
2016-11-21 15:01:18 -0800

    baseline working

--- noise.js
+++ noise.js
@@ -1,3 +1,61 @@
 $(document).ready( function() {
-  // your code here
+
+  var play = function(note) {
+    if (note == 'a') {
+      $('#aAudio').get(0).load();
+      $('#aAudio').get(0).play();
+    } else if (note == 'b') {
+      $('#bAudio').get(0).load();
+      $('#bAudio').get(0).play();
+    } else if (note == 'g') {
+      $('#gAudio').get(0).load();
+      $('#gAudio').get(0).play();
+    } else if (note == 'f') {
+      $('#fAudio').get(0).load();
+      $('#fAudio').get(0).play();
+    } else if (note == 'e') {
+      $('#eAudio').get(0).load();
+      $('#eAudio').get(0).play();
+    } else if (note == 'c') {
+      $('#cAudio').get(0).load();
+      $('#cAudio').get(0).play();
+    } else if (note == 'd') {
+      $('#dAudio').get(0).load();
+      $('#dAudio').get(0).play();
+    }
+  };
+
+  $( ".a" ).click(function() {
+    play("a");
+  });
+
+  $( ".c" ).click(function() {
+    play("c");
+  });
+
+  $( ".d" ).click(function() {
+    play("d");
+  });
+
+  $( ".e" ).click(function() {
+    play("e");
+  });
+
+  $( ".f" ).click(function() {
+    play("f");
+  });
+
+  $( ".g" ).click(function() {
+    play("g");
+  });
+
+  $( ".b" ).click(function() {
+    play("b");
+  });
+
+  $('body').keydown(function(event){
+    console.log(event.key);
+    play(event.key);
+  });
+
 });
