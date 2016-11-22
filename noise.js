73c4e6dc42d3bbbf9bb2d794c689df08cbcbfb4f
Melissa Rodriguez <olenellina@gmail.com>
2016-11-22 09:01:41 -0800

    further compressed

--- noise.js
+++ noise.js
@@ -1,29 +1,9 @@
 $(document).ready( function() {
 
   var play = function(note) {
-    if (note == 'a') {
-      // Loading the file first, ensures that the files can be played again
-      $('#aAudio').get(0).load();
-      $('#aAudio').get(0).play();
-    } else if (note == 'b') {
-      $('#bAudio').get(0).load();
-      $('#bAudio').get(0).play();
-    } else if (note == 'g') {
-      $('#gAudio').get(0).load();
-      $('#gAudio').get(0).play();
-    } else if (note == 'f') {
-      $('#fAudio').get(0).load();
-      $('#fAudio').get(0).play();
-    } else if (note == 'e') {
-      $('#eAudio').get(0).load();
-      $('#eAudio').get(0).play();
-    } else if (note == 'c') {
-      $('#cAudio').get(0).load();
-      $('#cAudio').get(0).play();
-    } else if (note == 'd') {
-      $('#dAudio').get(0).load();
-      $('#dAudio').get(0).play();
-    }
+    var tune = note + "Audio";
+    $(tune).get(0).load();
+    $(tune).get(0).play();
   };
 
   $( ".note" ).click(function() {
