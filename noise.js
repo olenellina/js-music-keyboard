2c829c145cee938e5ba79ac817a09fe40b081ebe
Melissa Rodriguez <olenellina@gmail.com>
2016-11-22 09:02:22 -0800

    added comment

--- noise.js
+++ noise.js
@@ -2,8 +2,9 @@ $(document).ready( function() {
 
   var play = function(note) {
     var tune = note + "Audio";
+    // Load is called first, to allow for multiple plays of a note
     $(tune).get(0).load();
-    $(tune).get(0).play();
+    $(tune).get(0).play();g
   };
 
   $( ".note" ).click(function() {
